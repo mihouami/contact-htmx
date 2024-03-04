55419d96f99b76e1069f17a834a18d72961a5270
Minou <am.mihoubi@gmail.com>
2024-03-04 23:29:34 +0100

    06-Delete contacts one by one

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("", index, name='index'),
-    path("add_contact", add_contact, name='add-contact'),
+    path("add_contact/", add_contact, name='add-contact'),
+    path("delete_contact/<int:pk>", delete_contact, name='delete_contact'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .forms import ContactForm
 from .models import Contact
+from django.views.decorators.http import require_http_methods
+
 
 def index(request):
     context = {'form':ContactForm(), 'contacts':Contact.objects.all()}
@@ -14,4 +16,14 @@ def add_contact(request):
             contact = form.save()
             return render(request, 'partials/contact.html', {'contact':contact})
     return render(request, 'partials/form.html', {'form':form})
+
+
+
+@require_http_methods(['DELETE'])
+def delete_contact(request, pk):
+    contact = Contact.objects.get(id=pk)
+    contact.delete()
+    response = HttpResponse(status=204)
+    response['HX-Trigger'] = 'delete'
+    return response
 
